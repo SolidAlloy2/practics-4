ec15a13a86183053ada03cc8538598a94c64ddd3
solidalloy2 <solidalloy@protonmail.com>
2024-10-19 21:50:43 +0300

    Uploaded the website

---
+++ tests.py
@@ -0,0 +1,22 @@
+import unittest
+from app import app
+
+
+class FlaskAppTests(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def test_home_page(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Welcome to the Home Page!', response.data)
+
+    def test_about_page(self):
+        response = self.app.get('/about')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'About Us', response.data)
+
+
+if __name__ == '__main__':
+    unittest.main()
